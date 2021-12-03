ae744c7716770380d106aaadc7e64564aa69f2a7
karamalmlahe <karamturi@hotmail.com>
2021-12-03 15:34:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const contactRoutes=require('./routes/contactRoutes')
 const Contact = require('./model/contactUs')
 
 mongoose.connect(dbConn,{useNewUrlParser: true, useUnifiedTopology: true})
-.then( result => {app.listen(3000)})
+.then( result => {app.listen(port)})
 .catch(err => {console.log('Not connected',err)})
 
  app.set('view engine','ejs')
